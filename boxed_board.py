034ee563763a1fa5c16b1a40eff0b288a0bf508d
Simon Kirkby <obeygiantrobot@gmail.com>
2018-05-23 10:33:13 +0800

    different boards inside a box

--- boxed_board.py
+++ boxed_board.py
@@ -7,6 +7,7 @@ from cqparts.constraint import Mate
 from cqparts.utils import CoordSystem
 
 from pizero import Pizero
+from arduino import Arduino 
 import case
 from case import Case
 import os
@@ -25,6 +26,7 @@ class BoxedBoard(cqparts.Assembly):
             'board': self.board,
         }
         # modify the box to fit the board
+
         self.case.length= self.board.length + 2*self.clearance
         self.case.width= self.board.width+ 2*self.clearance
         self.case.explode = 5
