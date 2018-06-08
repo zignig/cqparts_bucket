033ae66c48806024ef5d5fd99ab7817762742d23
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-08 11:41:46 +0800

    flat belt workingw

--- belt_test.py
+++ belt_test.py
@@ -9,25 +9,27 @@ from cqparts.utils.geometry import CoordSystem
 
 class Belt(cqparts.Part):
     # Parameters
-    rad = PositiveFloat(10)
-    spacing = PositiveFloat(40)
+    rad = PositiveFloat(5)
+    spacing = PositiveFloat(140)
 
-    belt_width = PositiveFloat(10)
-    belt_thickness = PositiveFloat(2)
+    belt_width = PositiveFloat(5)
+    belt_thickness = PositiveFloat(1)
 
     # default appearance
     #_render = render_props(template='tin')
     def profile(self):
-        pass
+        p = cq.Workplane("XZ").rect(self.belt_width,self.belt_thickness)
+        return p
 
     def make(self):
-        path = cq.Workplane("XY")\
-            .moveTo(self.rad,0)\
-            .lineTo(self.rad,self.spacing)\
-            .threePointArc((0,self.spacing+self.rad),(self.spacing,self.spacing))\
-            .lineTo(-self.rad,0)
-#            .radiusArc((0,self.rad),self.rad)
-        outer = cq.Workplane("XZ").circle(1).sweep(path,makeSolid=True)
+        p = self.profile()
+        outer = p.extrude(self.spacing)
+        p2 = self.profile().revolve(180,(2,self.rad),(1,self.rad))
+        outer = outer.union(p2)
+        p3 = self.profile().extrude(self.spacing).translate((0,0,2*self.rad))
+        outer = outer.union(p3)
+        p4 = self.profile().revolve(180,(-2,self.rad),(1,self.rad)).translate((0,-self.spacing,0))
+        outer = outer.union(p4)
 	return outer
 
 if __name__ == "__main__":
