0e924780709ba65842f9f95b3098ad8c6de961e4
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-08 10:58:37 +0800

    belt weirdnes

--- belt_test.py
+++ belt_test.py
@@ -10,20 +10,22 @@ from cqparts.utils.geometry import CoordSystem
 class Belt(cqparts.Part):
     # Parameters
     rad = PositiveFloat(10)
-    spacing = PositiveFloat(30)
+    spacing = PositiveFloat(40)
 
     belt_width = PositiveFloat(10)
     belt_thickness = PositiveFloat(2)
 
     # default appearance
     #_render = render_props(template='tin')
+    def profile(self):
+        pass
 
     def make(self):
         path = cq.Workplane("XY")\
-            .moveTo(0,self.rad)\
-            .lineTo(self.spacing,self.rad)\
-            .radiusArc((self.spacing,-self.rad),self.rad)
-#            .lineTo(0,-self.rad)
+            .moveTo(self.rad,0)\
+            .lineTo(self.rad,self.spacing)\
+            .threePointArc((0,self.spacing+self.rad),(self.spacing,self.spacing))\
+            .lineTo(-self.rad,0)
 #            .radiusArc((0,self.rad),self.rad)
         outer = cq.Workplane("XZ").circle(1).sweep(path,makeSolid=True)
 	return outer
---
+++ helix_test.py
@@ -0,0 +1,24 @@
+import cadquery as cq
+import cqparts
+from cadquery import Solid
+from cqparts.params import *
+from cqparts.display import render_props, display
+from cqparts.constraint import Fixed, Coincident
+from cqparts.constraint import Mate
+from cqparts.utils.geometry import CoordSystem
+
+class helix(cqparts.Part):
+    # Parameters
+    rad = PositiveFloat(10)
+    spacing = PositiveFloat(40)
+
+
+    def make(self):
+        helix = cq.Wire.makeHelix(pitch=1,height=1,radius=1)
+        sp = cq.Workplane("XY").circle(1).sweep(helix)
+	return h 
+
+if __name__ == "__main__":
+    from cqparts.display import display
+    B = helix()
+    display(B)
