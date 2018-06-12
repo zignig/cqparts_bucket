f27c6f30041b1a2441cd6ac0aafd7b994a27e00d
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-12 14:17:09 +0800

    rebuilding axis

--- cnc.py
+++ cnc.py
@@ -25,205 +25,86 @@ class PartRef(Parameter):
         return value
 
 
-class EndBlock(cqparts.Part):
-    height = PositiveFloat()
-    width = PositiveFloat()
-    length = PositiveFloat()
+# don't like it going to rebuild from scratch
 
+class _PlaceHolder(cqparts.Part):
+    length = PositiveFloat(10)
+    width = PositiveFloat(10)
+    height = PositiveFloat(10)
     def make(self):
-        wp = cq.Workplane("XY")
-        box = wp.box(self.length, self.width, self.height)
-        return box
+        return cq.Workplane("XY")\
+            .box(self.length,self.width,self.height)
 
-    @property
-    def mate_start(self):
-        return Mate(self, CoordSystem(
-            origin=(self.length/2, 0, 0),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-class Drive(cqparts.Part):
-    length = PositiveFloat()
-    width = PositiveFloat()
-    height = PositiveFloat()
-
-    def make(self):
-        wp = cq.Workplane("XY")
-        box = wp.box(self.length, self.width, self.height)
-        return box
-
-
-class Carriage(cqparts.Part):
-    height = PositiveFloat()
-    width = PositiveFloat()
-    length = PositiveFloat()
-    _render = render_props(template="green")
-
-    def make(self):
-        wp = cq.Workplane("XY")
-        box = wp.box(self.length, self.width, self.height)
-        return box
-
-    def mate_pos(self, pos=0):
-        return Mate(self, CoordSystem(
-            origin=(pos, 0, 0),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-
-class Rails(cqparts.Part):
-    height = PositiveFloat()
-    width = PositiveFloat()
-    length = PositiveFloat()
-    _render = render_props(template="blue")
-
-    def make(self):
-        wp = cq.Workplane("XY")
-        rails = wp.box(self.length, self.width, self.height)
-        return rails
-
-    @property
-    def mate_start(self):
-        return Mate(self, CoordSystem(
-            origin=(self.length/2, 0, 0),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-    @property
-    def mate_mid(self):
-        return Mate(self, CoordSystem(
-            origin=(0, 0, 0),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-    @property
-    def mate_end(self):
-        return Mate(self, CoordSystem(
-            origin=(-self.length/2, 0, 0),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-
-
-class DriveBlock(cqparts.Part):
-    height = PositiveFloat()
-    width = PositiveFloat()
-    length = PositiveFloat()
-    _render = render_props(template="steel")
-
-    def make(self):
-        wp = cq.Workplane("XY")
-        box = wp.box(self.length, self.width, self.height)
-        return box
-
-    @property
-    def mate_bottom(self):
-        return Mate(self, CoordSystem(
-            origin=(0, 0, -self.height/2),
-            xDir=(1, 0, 0),
-            normal=(0, 0, 1)
-        ))
-
-    @property
-    def mate_end(self):
-        return Mate(self, CoordSystem(
-            origin=(0, -self.length/2, self.height/2),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-    @property
-    def mate_start(self):
-        return Mate(self, CoordSystem(
-            origin=(-self.length/2, 0, 0),
-            xDir=(1, 0, 0),
-            normal=(0, 1, 0)
-        ))
-
-
-class Platform(cqparts.Part):
-    height = PositiveFloat(5)
-    width = PositiveFloat(200)
-    length = PositiveFloat(200)
-    def make(self):
-        wp = cq.Workplane("XY")
-        box = wp.box(self.length, self.width, self.height)
-        return box
-
-
-class Axis(cqparts.Assembly):
-    height = PositiveFloat(40)
-    width = PositiveFloat(200)
-    length = PositiveFloat(30)
-    axis_length = PositiveFloat(220)
-
-    drive_block  = PartRef(DriveBlock)
-    drive = PartRef(Drive)
-    rails = PartRef(Rails)
-    end_block = PartRef(EndBlock)
-    carriage = PartRef(Carriage)
-
-    pos = Float(0)
+# this is a base object to pass all the variables down
+class _AxisBase(cqparts.Assembly):
+    length = PositiveFloat(10)
+    width = PositiveFloat(10)
+    height = PositiveFloat(10)
 
     def make_components(self):
-        comp = {
-            'drive_block': self.drive_block(length=self.length,
-                                     width=self.width,
-                                     height=self.height),
-            'drive': self.drive(length=self.length,
-                                     width=self.width,
-                                     height=self.height),
-            'rails': self.rails(length=self.axis_length,
-                                width=self.width/2,
-                                height=self.height/2),
-            'end_block': self.end_block(length=self.length,
-                                 width=self.width,
-                                 height=self.height),
-            'carriage': self.carriage(length=self.length,
-                                      width=self.width,
-                                      height=self.height)
+        comps = {
+            'box' : _PlaceHolder(
+                length=self.length,
+                width=self.width,
+                height=self.height
+                )
         }
-        return comp
+        return comps
+
+    def make_constraints(self):
+        return [
+            Fixed(self.components['box'].mate_origin),
+        ]
+
+
+class DriveEnd(_AxisBase):
+    pass
+
+class IdleEnd(_AxisBase):
+    pass
+
+class Drive(_AxisBase):
+    pass
+
+class Rails(_AxisBase):
+    pass
+
+class Carriage(_AxisBase):
+    pass
+
+class Axis(_AxisBase):
+    drive_end = PartRef(DriveEnd)
+    idle_end = PartRef(IdleEnd)
+    drive = PartRef(Drive)
+    rails = PartRef(Rails)
+    carriage = PartRef(Carriage)
+
+    length = PositiveFloat(150)
+    width = PositiveFloat(75)
+    height = PositiveFloat(40)
+
+    pos = PositiveFloat(0)
+
+    def make_components(self):
+        comps = {
+            'drive_end' : self.drive_end(width=self.width),
+            'idle_end' : self.idle_end(width=self.width),
+            'drive': self.drive(),
+            'rails' : self.rails(),
+            'carriage' : self.carriage()
+        }
+        return comps
 
     def make_constraints(self):
         constr = [
-            Fixed(self.components['drive_block'].mate_origin),
-            Coincident(self.components['drive'].mate_start,
-                       self.components['drive_block'].mate_start),
-            Coincident(self.components['rails'].mate_start,
-                       self.components['drive_block'].mate_start),
-            Coincident(self.components['carriage'].mate_pos(self.pos),
-                       self.components['rails'].mate_mid),
-            Coincident(self.components['end_block'].mate_start,
-                       self.components['rails'].mate_end)
+            Fixed(self.components['drive_end'].mate_origin),
+            Fixed(self.components['idle_end'].mate_origin
+                ,CoordSystem((self.length,0,0),(1,0,0),(0,0,1)))
         ]
         return constr
 
-
-class OtherRails(Rails):
-    def make(self):
-        wp = cq.Workplane("ZY")
-        tube = wp.circle(self.height/2).extrude(self.length)
-        tube = tube.translate((self.length/2, 0, 0))
-        return tube
-
-
-class OtherDrive(DriveBlock):
-
-    _render = render_props(color=(50,50,50))
-
-    def make(self):
-        c = super(OtherDrive, self).make()
-        c = c.faces(">Z").shell(-7).chamfer(0.5)
-        return c
-
 if __name__ == "__main__":
     from cqparts.display import display
-    e = Axis(pos=0,axis_length=200)
-    #e = Platform()
+    #e = Axis(pos=0,axis_length=200)
+    e = Axis()
     display(e)
