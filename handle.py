09fc5f3e6543de26efc41c3e959d964b58ef7d18
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-13 12:42:22 +0800

    working handle ( use tangent code elsewhere )

--- handle.py
+++ handle.py
@@ -27,24 +27,40 @@ def CalcTangents(p1, r1, p2, r2):
     y3 = y1 + r1 * math.sin(half_pi-alpha)
     x4 = x2 + r2 * math.cos(half_pi-alpha)
     y4 = y2 + r2 * math.sin(half_pi-alpha)
-    return ((x3, y3), (x4, y4))
+    return (x3, y3), (x4, y4)
 
 
 class Handle(cqparts.Part):
-    length = PositiveFloat(50)
-    rad1 = PositiveFloat(5)
-    rad2 = PositiveFloat(10)
-    thickness = PositiveFloat(10)
+    length = PositiveFloat(100)
+    rad1 = PositiveFloat(30)
+    rad2 = PositiveFloat(15)
+    thickness = PositiveFloat(15)
+    hole = PositiveFloat(8)
+    handle_height = PositiveFloat(30)
 
     def make(self):
-        b = cq.Workplane("XY").circle(self.rad2).extrude(self.thickness)
+        b = cq.Workplane("XY").circle(self.rad1).extrude(self.thickness)
         b2 = cq.Workplane("XY")\
-            .circle(self.rad1)\
+            .circle(self.rad2)\
             .extrude(self.thickness)\
             .translate((self.length, 0, 0))
         b = b.union(b2)
+        # generate the tangents
+        p1 , p2 = CalcTangents((0, 0), self.rad1, (self.length,0), self.rad2)
+        pts = [ p1 , (p1[0],-p1[1]), (p2[0],-p2[1]) ,p2 , p1 ]
+        print pts
+        base = cq.Workplane("XY").polyline(pts).close().extrude(self.thickness)
+        b = b.union(base)
+        handle = cq.Workplane("XY")\
+            .circle(self.hole)\
+            .extrude(self.handle_height)\
+            .translate((self.length,0,self.thickness))
+        b = b.union(handle)
+        b = b.faces("|Z").chamfer(1)
+        h = cq.Workplane("XY").circle(self.rad2*0.8).extrude(self.thickness)
+        b = b.cut(h)
         return b
-        
+
 
 if __name__ == "__main__":
     from cqparts.display import display
