ff03982fec46dca51be4894aab65dd42a861be7d
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-19 16:48:28 +0800

    build scene from scratch

--- blender/render_test.py
+++ blender/render_test.py
@@ -4,10 +4,23 @@
 import bpy
 # snippit , not working script
 
+# maybe script build the entire scene
+bpy.ops.scene.new(type='NEW')
+bpy.context.scene.name = 'cqparts'
+# make the world
+bpy.ops.world.new()
+bpy.data.worlds['World.001'].name = 'NewWorld'
+
 # make default scene
+bpy.ops.object.camera_add()
+bpy.ops.object.lamp_add(type='POINT')
+# turn on ambient occlusion
+
+# 
 bpy.ops.import_scene.gltf(filepath="/opt/stash/out.gltf")
 # position camera and stuff
-bpy.data.scenes['Scene'].render.filepath = "/opt/stash/image.png"
-bpy.data.scenes['Scene'].objects['out'].select = True
+theScene = bpy.data.scenes['cqparts']
+theScene.render.filepath = "/opt/stash/image.png"
+theScene.objects['out'].select = True
 bpy.ops.transform.resize(value=(100,100,100))
 bpy.ops.render.render(write_still=True)
