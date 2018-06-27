cb183ac1c36f1bf42904e01087b9ef60ff31b8b6
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-27 15:14:02 +0000

    moved standoff and screw into mounted, BBB , holes  still weird

--- pizero.py
+++ pizero.py
@@ -60,6 +60,24 @@ class BeagleBoneBlack(PCBBoard):
     width = PositiveFloat(54.61)
     thickness = PositiveFloat(1)
     corner_radius = PositiveFloat(12.7)
+    corner_radius2 = PositiveFloat(6.35)
+    hole_size = PositiveFloat(4.45)
+
+    # This returns the verts that the screws get aligned to
+    def mount_points(self,offset=0):
+        pts = [ (14.61,3.18), (14.61,3.18+48.39), (14.61+66.10,6.35), (14.61+66.10,6.35+41.91) ]
+        wp = cq.Workplane("XY",origin=(-self.length/2,-self.width/2,offset))
+        h = wp.moveTo(*pts[0]).polyline(pts[1:]).vertices()
+        return h
+
+    def make(self):
+        wp = cq.Workplane("XY")
+        board = wp.box(length=self.length,width=self.width,height=self.thickness)
+        board.edges("|Z and  >X").fillet(self.corner_radius)
+        board.edges("|Z and  <X").fillet(self.corner_radius2)
+        holes =  self.mount_points(offset=self.thickness*2).circle(self.hole_size/2).extrude(-self.thickness*4)
+        board = board.cut(holes)
+        return board
 
 if __name__ == "__main__":
     from cqparts.display import display
