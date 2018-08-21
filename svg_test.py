c2fa9bbccba6ba480cebda47266079a502b76c76
Simon Kirkby <obeygiantrobot@gmail.com>
2018-08-21 11:10:40 +0800

    working svg layout

--- svg_test.py
+++ svg_test.py
@@ -1,22 +1,44 @@
 import flip_box
+import box
 #import pencil_case
 import SVGexport
 import cadquery as cq
 
-fb = flip_box.FlipBox(height=40,outset=4)
-#fb = pencil_case.PencilCase()
-#top = fb.components['top']
-#left = fb.components['left']
-
+fb = flip_box.FlipBox(height=60,outset=3,thickness=3)
+#fb = box.Boxen() 
 wp = cq.Workplane("XY")
-#wp = wp.union(top.local_obj)
-#wp = wp.union(left.local_obj.translate((fb.length*2,0,0,)))
 
-counter = 0 
+y = 0 
+x = 0
+gap = 5.0
+widths = [0]
+names = []
+offset = 0  
+offsets = [0]
+rects = []
+# generate offsets
+outset = 2*fb.outset
 for i in fb.components:
-    print(counter,i)
-    wp = wp.union(fb.components[i].local_obj.translate((120*counter,0,0)))
-    counter += 1
+#    w  = fb.components[i].width+outset
+#    l  = fb.components[i].length+outset
+    w  = fb.components[i].bounding_box.xlen
+    l  = fb.components[i].bounding_box.ylen
+    widths.append(w)
+    names.append(i)
+    offset += w 
+    offsets.append(offset)
+    rects.append((w,l))
+    
+offsets.append(offset+gap)
+print(names)
+print(widths)
+print(offsets)
+print(rects)
+
+for i,j in enumerate(names):
+    print(i,j,widths[i],offsets[i])
+    o = offsets[i]/2.0 + offsets[i+1]/2.0 + i*gap
+    wp = wp.union(fb.components[names[i]].local_obj.translate((o,0,0)))
 
 SVGexport.exportSVG(wp,'box.svg')
 
