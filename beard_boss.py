0f1f399502a3fd1506a45018f8c35e0c720190e6
Simon Kirkby <obeygiantrobot@gmail.com>
2018-08-25 11:50:59 +0800

    beard boss

---
+++ beard_boss.py
@@ -0,0 +1,62 @@
+" A cqparts version of the beard boss" 
+" from https://groups.google.com/forum/#!topic/cadquery/_jCq0z9Swio"
+
+import cadquery as cq
+import cqparts
+from cqparts.params import *
+
+class _boss(cqparts.Part):
+    boss_height = PositiveFloat(10)
+    boss_diameter = PositiveFloat(10)
+    def make(self):
+        wp = cq.Workplane("XY").circle(self.boss_diameter/2).extrude(self.boss_height)
+        return wp
+
+class BeardBoss(cqparts.Part):
+    length = PositiveFloat(50)
+    width = PositiveFloat(50)
+    height = PositiveFloat(6)
+
+
+    boss_height = PositiveFloat(10)
+    boss_diameter = PositiveFloat(15)
+    hole_diameter = PositiveFloat(5)
+
+    x_spacing = PositiveFloat(30)
+    y_spacing = PositiveFloat(30)
+
+    # hand back vertices for the boss positions
+    def mount_points(self,offset=0):
+        wp = cq.Workplane("XY",origin=(0,0,offset))
+        h = wp.rect(self.x_spacing
+                    ,self.y_spacing
+                    ,forConstruction=True).vertices()
+        return h.objects
+
+
+    def make(self):
+        # base plate
+        pl  = cq.Workplane("XY").box(self.length,self.width,self.height)
+        pl = pl.translate((0,0,self.height/2))
+        pl = pl.edges("|Z").fillet(3)
+        # add the bosses
+        mp = self.mount_points()
+        for i in mp:
+            b = _boss(
+                    boss_height=self.boss_height,
+                    boss_diameter=self.boss_diameter
+                    )
+            b = b.local_obj.translate((i.X,i.Y,self.height))
+            pl = pl.union(b)
+        # cur the holes
+        for i in mp:
+            h  = cq.Workplane("XY").circle(self.hole_diameter/2).extrude(self.height+self.boss_height)
+            h = h.translate((i.X,i.Y,0))
+            pl = pl.cut(h)
+        pl = pl.faces(">Z[1]").edges("not(<X or >X or <Y or >Y)").fillet(1)
+        return pl 
+
+if __name__ == "__main__":
+    from cqparts.display import display
+    bb = BeardBoss()
+    display(bb)
