8f8f175d5d7d8393f1699f55abd466944e09e55a
Simon Kirkby <obeygiantrobot@gmail.com>
2019-02-18 14:04:49 +0800

    start of BB extraviganza

--- multi.py
+++ multi.py
@@ -7,6 +7,42 @@ from cqparts.constraint import Mate
 from cqparts.utils import CoordSystem
 
 
+class Gallery(cqparts.Assembly):
+    offset = PositiveFloat(60)
+
+    def initialize_parameters(self):
+        self.coll = []
+
+    def add(self, i):
+        self.coll.append(i)
+
+    @classmethod
+    def item_name(cls, index):
+        return "item_%03i" % index
+
+    def make_components(self):
+        items = {}
+        for i in range(len(self.coll)):
+            items[self.item_name(i)] = self.coll[i]
+        return items
+
+    def make_constraints(self):
+        constraints = []
+        for i in self.coll:
+            print(i.bounding_box.wrapped)
+        #for i in range(len(self.coll)):
+        #    constraints.append(
+        #        Fixed(
+        #            self.coll[i].mate_origin,
+        #            CoordSystem(
+        #                origin=(0, i * self.offset - (total / 2) + self.offset / 2, 0),
+        #                xDir=(1, 0, 0),
+        #                normal=(0, 0, 1),
+        #            ),
+        #        )
+        #    )
+        return constraints
+
 class Arrange(cqparts.Assembly):
     offset = PositiveFloat(60)
 
--- servo_horns.py
+++ servo_horns.py
@@ -11,13 +11,13 @@ from cqparts.constraint import Mate
 from cqparts.utils.geometry import CoordSystem
 from cqparts.search import register
 
-from .multi import Arrange
+from .multi import Arrange, Gallery
 from cqparts_motors.shaft import Shaft
 
 from .calculations import CalcTangents
 
 
-class ShowHorns(Arrange):
+class ShowHorns(Gallery):
     pass
 
 
